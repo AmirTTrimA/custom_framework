93bd9b214d6a5b079ccf70cfe3b1302ffde0823d
FATEMEfati <fatemenoormohammady.intp@gmail.com>
2024-10-02 11:49:49 -0700

    session.py

--- session/session.py
+++ session/session.py
@@ -1,45 +1,42 @@
 import hashlib
-import random
 import json
+import random
+import sys
+import os
+sys.path.append(os.path.abspath('C:\\Users\\BEROOZDG\\OneDrive\\Desktop\\مکتب\\project\\framework'))
 from app.models import Session as SessionModel
 from datetime import datetime,timedelta
 from sqlalchemy.orm import Session
-from app.migartions import engine
-from app.models import User,Address
+from app.migrathons import engine
 from sqlalchemy import select
 
-def generate_session_id():
 
+def generate_session_id():
     return hashlib.sha256(f'{random.random()}'.encode()).hexdigest()
 
-def creat_session(user_id):
-
-    session_id =  generate_session_id()
-    session_value = json.dumps({'user_id': user_id})
-
+def create_session(user_id):
     with Session(engine) as session:
-        
-        session_model = SessionModel(
-            session_id=session_id,
+        session_id= generate_session_id()
+        session_value =json.dumps([{'user_id': user_id}])
+        now=datetime.now()
+        two_days=timedelta(days=2)
+        session_model=SessionModel(
+            session_key=session_id,
             session_value=session_value,
-            expire_date=datetime.now()+timedelta(days=2))
-    
+            expire_date=now+two_days
+    )
         session.add(session_model)
         session.commit()
     return session_id
 
 def get_session(session_id):
     with Session(engine) as session:
-
         query = select(SessionModel).where(
-            SessionModel.session_id == session_id)
-        session_model = session.scalar(query)
+            SessionModel.session_key==session_id
+        )
+        session_model=session.scalar(query)
         if session_model is None:
             return None
         elif session_model.expire_date < datetime.now():
             return None
-    return json.loads(session_model,session_value)
-
-def destroy_session(session_id):
-    if session_id in Session:
-        del Session[session_id]
+    return json.loads(session_model.session_value)
